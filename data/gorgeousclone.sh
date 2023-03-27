f5beedc46e701d87cffac48b9278157c1f6f526d
Erka Merf <68033864+erkamerf@users.noreply.github.com>
2023-03-27 16:38:49 -0400

    adding script for cloning character specific swap scripts

---
+++ data/gorgeousclone.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/erkamerf/Harissa.git
+git clone https://github.com/erkamerf/Gorgeousgeorge.git
+git clone https://github.com/erkamerf/Emeliana.git
+git clone https://github.com/erkamerf/Deitmund.git
+
